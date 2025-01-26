d86a3eeb18883658dfbe76e0830821d521b65991
adefemi171 <adefemi171@gmail.com>
2025-01-26 18:44:44 +0100

    add adot exporter module

---
+++ aws_adot_exporter/main.tf
@@ -0,0 +1,9 @@
+module "aws_adot_exporter" {
+  source               = "../helm_release"
+  chart_namespace      = var.aws_adot_exporter_namespace
+  chart_release_name   = var.aws_adot_exporter_release_name
+  chart_repository_url = "https://aws.github.io/aws-otel-collector-helm"
+  chart_name           = "aws-otel-collector"
+  chart_version        = var.aws_adot_exporter_chart_version
+  chart_values         = var.aws_adot_exporter_values
+}
